f64f56297f82238531c3f5428b7054bf90608b4a
Halo-CHN <halo-chn@outlook.com>
2015-07-06 18:11:57 +0800

    little edit

--- TwiceConfirmDemo/src/chn/halo/twiceconfirmdemo/util/TwiceConfirmExitUtil.java
+++ TwiceConfirmDemo/src/chn/halo/twiceconfirmdemo/util/TwiceConfirmExitUtil.java
@@ -60,13 +60,12 @@ public class TwiceConfirmExitUtil {
 	 *            当前应用中已经打开的Activity
 	 */
 	public void showToast(Context context, List<? extends Activity> activities) {
-		if (time == 0) {//计数为0，重新开始计时
-			toast = Toast.makeText(context, "再按一次返回将退出程序", FINALSECOND * 1000);
+		if (time == 0) {// 计数为0，重新开始计时
+			toast = Toast.makeText(context.getApplicationContext(), "再按一次返回将退出程序", FINALSECOND * 1000);
 			toast.show();
 			timeGos();
-		} else {//计数不为0，说明在限定时间内在此按了返回键，退出程序
+		} else {// 计数不为0，说明在限定时间内在此按了返回键，退出程序
 			timer = null;
-			toast.cancel();
 			// finish掉所有已经打开的Activity
 			if (null != activities && activities.size() > 0) {
 				for (Activity ac : activities) {
